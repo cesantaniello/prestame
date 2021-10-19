6e4960aaaf7fffb4b3032543f67f5dce5e4f957f
Administrator <admin@example.com>
2021-10-19 16:29:06 +0200

    10. Muestra mensaje de error

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const Formulario = ({cantidad, guardarCantidad, plazo, guardarPlazo}) => {
 
@@ -12,50 +12,57 @@ const Formulario = ({cantidad, guardarCantidad, plazo, guardarPlazo}) => {
         //Validar
         if(cantidad === 0 || plazo === ''){
             guardarError(true);
+            return;
         }
+
+        //Eliminar el error previo
+        guardarError(false);
 
         //Realizar el calculo
     
     }
 
     return ( 
-        <form onSubmit={calcularPrestamo}>
-            <div className="row">
-                <div>
-                    <label>Cantidad Prestamo</label>
-                    <input 
-                        className="u-full-width" 
-                        type="number" 
-                        placeholder="Ejemplo: 3000"
-                        onChange={
-                            e => guardarCantidad(parseInt(e.target.value))
-                        } 
-                    />
+        <Fragment>
+            <form onSubmit={calcularPrestamo}>
+                <div className="row">
+                    <div>
+                        <label>Cantidad Prestamo</label>
+                        <input 
+                            className="u-full-width" 
+                            type="number" 
+                            placeholder="Ejemplo: 3000"
+                            onChange={
+                                e => guardarCantidad(parseInt(e.target.value))
+                            } 
+                        />
+                    </div>
+                    <div>
+                        <label>Plazo para Pagar</label>
+                        <select 
+                            className="u-full-width"
+                            onChange={
+                                e => guardarPlazo(parseInt(e.target.value))
+                            }
+                        >
+                            <option value="">Seleccionar</option>
+                            <option value="3">3 meses</option>
+                            <option value="6">6 meses</option>
+                            <option value="12">12 meses</option>
+                            <option value="24">24 meses</option>
+                        </select>
+                    </div>
+                    <div>
+                        <input 
+                            type="submit" 
+                            value="Calcular" 
+                            className="button-primary u-full-width" 
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>Plazo para Pagar</label>
-                    <select 
-                        className="u-full-width"
-                        onChange={
-                            e => guardarPlazo(parseInt(e.target.value))
-                        }
-                    >
-                        <option value="">Seleccionar</option>
-                        <option value="3">3 meses</option>
-                        <option value="6">6 meses</option>
-                        <option value="12">12 meses</option>
-                        <option value="24">24 meses</option>
-                    </select>
-                </div>
-                <div>
-                    <input 
-                        type="submit" 
-                        value="Calcular" 
-                        className="button-primary u-full-width" 
-                    />
-                </div>
-            </div>
-        </form>
+            </form>
+            {error ? <p className="error">Todos los campos son obligatorios</p> : null}
+        </Fragment>
     );
 }
 
